c11bb6bf70c8cb626fdb524dccdd8952c12dde88
arunma076 <arunma076@gmail.com>
2024-07-19 17:12:23 +0530

    new files

---
+++ promtail/promtail.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status.
+
+wget https://github.com/grafana/loki/releases/download/v2.9.0/promtail-linux-amd64.zip
+unzip promtail-linux-amd64.zip
+sudo mv promtail-linux-amd64 /usr/local/bin/promtail
+sudo chmod +x /usr/local/bin/promtail
+
+sudo mkdir -p /etc/promtail/
+sudo mv promtail-local-config.yaml /etc/promtail/	
+
+sudo mv promtail.service /etc/systemd/system/
+
+sudo systemctl daemon-reload 
+sudo systemctl start promtail.service 
+sudo systemctl enable promtail.service 
+sudo systemctl status promtail.service
