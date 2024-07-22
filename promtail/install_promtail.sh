47acb5074b0665586c7334c56fbf6aa036c6fcb7
arunma076 <arunma076@gmail.com>
2024-07-22 12:04:59 +0530

    new update

--- promtail/install_promtail.sh
+++ promtail/install_promtail.sh
@@ -8,11 +8,14 @@ sudo mv promtail-linux-amd64 /usr/local/bin/promtail
 sudo chmod +x /usr/local/bin/promtail
 
 sudo mkdir -p /etc/promtail/
-sudo mv promtail-local-config.yaml /etc/promtail/	
+sudo mv promtail-local-config.yaml /etc/promtail/
+
+SERVER_NAME=""
+sed -i "s/server_name: .*/server_name: ${SERVER_NAME}/g" /etc/promtail/promtail-local-config.yaml
 
 sudo mv promtail.service /etc/systemd/system/
 
-sudo systemctl daemon-reload 
-sudo systemctl start promtail.service 
-sudo systemctl enable promtail.service 
+sudo systemctl daemon-reload
+sudo systemctl start promtail.service
+sudo systemctl enable promtail.service
 sudo systemctl status promtail.service
